94f3db8ecdedbe39299cd1529f62a44a75b7e070
elio32 <eliosinaj@yahoo.com>
2023-05-31 22:20:00 +0200

    Project Update

--- src/main/java/com/project/BookingFlight/model/entity/Flight.java
+++ src/main/java/com/project/BookingFlight/model/entity/Flight.java
@@ -19,38 +19,54 @@ public class Flight {
 
     @Column(name = "airlineCode",nullable = false)
     private String airlineCode;
+
     @Column(name = "flightNumber",nullable = false,unique = true)
     private String flightNumber;
+
     @Column(name = "origin",nullable = false)
     private String origin;
+
     @Column(name = "destination",nullable = false)
     private String destination;
+
     @Column(name = "departuresDate",nullable = false)
     @Temporal(value = TemporalType.DATE)
     private Date departureDate;
+
     @Column(name = "arrivaldATE",nullable = false)
     @Temporal(value = TemporalType.DATE)
     private Date arrivalDate;
+
     @Column(name = "departureTime",nullable = false)
     private LocalTime departureTime;
+
     @Column(name = "arrivalTime",nullable = false)
     private LocalTime arrivalTime;
+
     @Column(name = "price",nullable = false)
     private Double price;
+
     @Column(name = "totalEconomySeats",nullable = false)
     private Integer totalEconomySeats;
+
     @Column(name = "totalPremiumEconomySeats",nullable = false)
     private Integer totalPremiumEconomySeats;
+
     @Column(name = "totalBusinessSeats",nullable = false)
     private Integer totalBusinessSeats;
+
     @Column(name = "totalFirstClassSeats",nullable = false)
     private Integer totalFirstClassSeats;
+
     @Column(name = "availableEconomySeats",nullable = false)
     private Integer availableEconomySeats;
+
     @Column(name = "availablePremiumEconomySeats",nullable = false)
     private Integer availablePremiumEconomySeats;
+
     @Column(name = "availableBusinessSeats",nullable = false)
     private Integer availableBusinessSeats;
+
     @Column(name = "availableFirstClassSeats",nullable = false)
     private Integer availableFirstClassSeats;
 
